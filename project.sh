ad6c26ad71d5a1d17a64308f644a7f6318aa0a0c
Tom Softreck <tom@sapletta.com>
2026-01-26 09:25:12 +0100

    draft

--- project.sh
+++ project.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-code2logic ./ -f toon --compact --function-logic --with-schema -o project.toon
+code2logic ./src -f toon --compact --function-logic --with-schema -o project.toon
